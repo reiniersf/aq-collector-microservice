6545c8389e26d0ffc1e9e08e10b7520ad4b59b83
Silverio <silverio.reinier@experian.com>
2020-02-28 18:27:15 +0200

    Add the transformer [JsonNode -> AQLog] function

--- src/main/java/org/alterbg/musala/aq/components/AQDataTransformer.java
+++ src/main/java/org/alterbg/musala/aq/components/AQDataTransformer.java
@@ -1,8 +1,16 @@
 package org.alterbg.musala.aq.components;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import org.alterbg.musala.aq.bean.AQLog;
+import org.alterbg.musala.aq.bean.GLocation;
+import org.alterbg.musala.aq.bean.MeasureUnit;
+import org.alterbg.musala.aq.bean.Particle;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AQDataTransformer {
 
+  public AQLog toAQLog(JsonNode aqForecast) {
+    return new AQLog(new GLocation(-23.8351, 151.254), MeasureUnit.µgm3, Particle.co, 10);
+  }
 }
