d31d4915544908301e18542ee5f1df69972e2263
Silverio <silverio.reinier@experian.com>
2020-02-28 15:35:25 +0200

    Enable scheduled beans in the application

--- src/main/java/org/alterbg/musala/aq/AQCollectorApplication.java
+++ src/main/java/org/alterbg/musala/aq/AQCollectorApplication.java
@@ -2,8 +2,10 @@ package org.alterbg.musala.aq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AQCollectorApplication {
 
   public static void main(String[] args) {
