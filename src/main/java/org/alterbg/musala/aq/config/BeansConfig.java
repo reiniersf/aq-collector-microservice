f22bacb392df3f772da12aa51c470913a291c436
Silverio <silverio.reinier@experian.com>
2020-02-28 18:28:00 +0200

    Clean up imports

--- src/main/java/org/alterbg/musala/aq/config/BeansConfig.java
+++ src/main/java/org/alterbg/musala/aq/config/BeansConfig.java
@@ -1,17 +1,10 @@
 package org.alterbg.musala.aq.config;
 
-import java.util.HashMap;
-import java.util.Map;
 import okhttp3.OkHttpClient.Builder;
-import org.alterbg.musala.aq.bean.AQLog;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
