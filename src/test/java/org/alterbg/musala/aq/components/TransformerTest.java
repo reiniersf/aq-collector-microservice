aa96ca37c019d3c8032bc261bc42b933944d39e1
Silverio <silverio.reinier@experian.com>
2020-03-04 11:09:58 +0200

    Set display name for each test

--- src/test/java/org/alterbg/musala/aq/components/TransformerTest.java
+++ src/test/java/org/alterbg/musala/aq/components/TransformerTest.java
@@ -3,6 +3,8 @@ package org.alterbg.musala.aq.components;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.alterbg.musala.aq.AQCollectorApplication;
+import org.alterbg.musala.aq.components.collector.AQDataTransformer;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +16,7 @@ public class TransformerTest {
   private AQDataTransformer transformer;
 
   @Test
+  @DisplayName("Transformer creation")
   void shouldCreateTransformer() {
     assertThat(transformer).isNotNull();
   }
