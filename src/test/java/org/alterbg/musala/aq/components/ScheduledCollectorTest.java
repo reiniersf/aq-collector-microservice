3540d6602aa6e19527467a3b33f632e9a67cd060
Silverio <silverio.reinier@experian.com>
2020-03-09 20:10:41 +0200

    Optimize imports for test

--- src/test/java/org/alterbg/musala/aq/components/ScheduledCollectorTest.java
+++ src/test/java/org/alterbg/musala/aq/components/ScheduledCollectorTest.java
@@ -4,8 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
 import java.time.Duration;
-import org.alterbg.musala.aq.components.collector.AQScheduledCollector;
 import org.alterbg.musala.aq.ScheduledTestConfig;
+import org.alterbg.musala.aq.components.collector.AQScheduledCollector;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
